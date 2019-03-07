68ff8ac6378566f2b03ddd17ba27360a07499ec4
Philip Pan <philip@Philips-MacBook-Pro-2.local>
2019-03-07 12:58:01 -0800

    Changed server port to 80

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hjs')
 
 
-app.listen(5000)
-console.log('Server running on http://localhost:5000')
+app.listen(80)
+console.log('Server running on http://localhost:80')
 console.log(process.env.DB_HOST)
